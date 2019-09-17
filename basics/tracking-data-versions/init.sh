9256c5f3704e61ffbdbb8e5ed007da96f35d3078
Dashamir Hoxha <dashohoxha@gmail.com>
2019-09-17 20:17:38 +0200

    Install highlight

--- basics/tracking-data-versions/init.sh
+++ basics/tracking-data-versions/init.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# prompt
-PS1='\033[01;34m\w\033[00m$ \033[01;32m'
-trap 'echo -ne "\033[00m"' DEBUG
-
 # git
 git config --global user.email "guest@example.com"
 git config --global user.name "Guest User"
 git config color.status always
 git config color.ui always
+
+# install highlight
+apt install --yes highlight
+
+# prompt
+PS1='\033[01;34m\w\033[00m$ \033[01;32m'
+trap 'echo -ne "\033[00m"' DEBUG
 
 # dvc
 pip3 install dvc
