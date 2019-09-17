038a844cf2554f115f3e2a7bd1cdaffd759eff8a
Dashamir Hoxha <dashohoxha@gmail.com>
2019-09-17 20:21:02 +0200

    Fix init.sh

--- basics/tracking-data-versions/init.sh
+++ basics/tracking-data-versions/init.sh
@@ -6,9 +6,6 @@ git config --global user.name "Guest User"
 #git config color.status always
 #git config color.ui always
 
-# install highlight
-apt install --yes highlight
-
 # prompt
 PS1='\033[01;34m\w\033[00m$ \033[01;32m'
 trap 'echo -ne "\033[00m"' DEBUG
@@ -16,3 +13,6 @@ trap 'echo -ne "\033[00m"' DEBUG
 # dvc
 pip3 install dvc
 dvc version
+
+# install highlight
+apt install --yes highlight
