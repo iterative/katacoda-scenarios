3e14b04d9367333544ab5e969c73e3affad3447d
Dashamir Hoxha <dashohoxha@gmail.com>
2019-10-07 18:05:10 +0200

    Init script breaks, trying to fix it

--- get-started/tracking/init.sh
+++ get-started/tracking/init.sh
@@ -17,7 +17,7 @@ trap 'echo -ne "\033[00m"' DEBUG
 pip3 install dvc
 
 # install highlight and dos2unix
-apt install --yes highlight dos2unix
+apt install --yes highlight dos2unix || true
 
 # clear screen
 clear
