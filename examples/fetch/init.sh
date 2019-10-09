64691c8869a2b4a916facb6dce22fbf78ac1cd2e
Dashamir Hoxha <dashohoxha@gmail.com>
2019-10-09 23:07:15 +0200

    test

--- examples/fetch/init.sh
+++ examples/fetch/init.sh
@@ -10,8 +10,8 @@ git config --global user.email "guest@example.com"
 git config --global user.name "Guest User"
 
 # prompt
-PS1='\033[01;34m\w\033[00m$ \033[01;32m'
-trap 'echo -ne "\033[00m"' DEBUG
+#PS1='\033[01;34m\w\033[00m$ \033[01;32m'
+#trap 'echo -ne "\033[00m"' DEBUG
 
 # clear the screen
 clear
