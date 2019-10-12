0c93c2e2b6db6d9e7367368e89c6c8665224dd23
Dashamir Hoxha <dashohoxha@gmail.com>
2019-10-12 11:16:36 +0200

    No need for installing highlight

--- basics/importing/install.sh
+++ basics/importing/install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# install highlight
-apt install --yes highlight
-
 # install bash completion for dvc
 wget -O /etc/bash_completion.d/dvc \
     https://raw.githubusercontent.com/iterative/dvc/master/scripts/completion/dvc.bash
