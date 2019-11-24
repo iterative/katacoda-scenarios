9518ecd335c461faebe8ac47d7405b079d3dbaed
Dashamir Hoxha <dashohoxha@gmail.com>
2019-11-24 19:32:14 +0100

    ssh component is not needed

--- examples/mounted-storage/install.sh
+++ examples/mounted-storage/install.sh
@@ -26,4 +26,4 @@ wget -O /etc/bash_completion.d/dvc \
     https://raw.githubusercontent.com/iterative/dvc/master/scripts/completion/dvc.bash
 
 # install dvc
-pip3 install 'dvc[ssh]'
+pip3 install dvc
