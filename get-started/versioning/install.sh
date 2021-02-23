2687ff8eb3082eae74222fccd2fc3a8fdfd1dfbb
Emre Sahin <github@emresult.com>
2021-02-23 15:56:45 +0300

    modified installation script to use snap. it should be faster

--- get-started/versioning/install.sh
+++ get-started/versioning/install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # install dvc
-sudo wget https://dvc.org/deb/dvc.list \
-          -O /etc/apt/sources.list.d/dvc.list
-sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/dvc.list" \
-    -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
-sudo apt install dvc
+# sudo wget https://dvc.org/deb/dvc.list \
+#           -O /etc/apt/sources.list.d/dvc.list
+# sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/dvc.list" \
+#     -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+# sudo apt install dvc
+
+# instead of apt, use snap, it should be faster
+
+snap install dvc
 
 # install bash completion for dvc
 dvc completion -s bash > /etc/bash_completion.d/dvc
