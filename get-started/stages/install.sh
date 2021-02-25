54f2bee398929c003def034b0930dd04b638bf61
Emre Sahin <github@emresult.com>
2021-02-25 16:42:51 +0300

    changed install command to snap

--- get-started/stages/install.sh
+++ get-started/stages/install.sh
@@ -4,9 +4,11 @@
 apt install --yes highlight
 
 # install bash completion for dvc
-wget -O /etc/bash_completion.d/dvc \
-    https://raw.githubusercontent.com/iterative/dvc/master/scripts/completion/dvc.bash
+# wget -O /etc/bash_completion.d/dvc \
+#     https://raw.githubusercontent.com/iterative/dvc/master/scripts/completion/dvc.bash
 
 # install dvc
-pip3 install --ignore-installed PyYAML
-pip3 install dvc
+# pip3 install --ignore-installed PyYAML
+# pip3 install dvc
+
+snap install --classic dvc
