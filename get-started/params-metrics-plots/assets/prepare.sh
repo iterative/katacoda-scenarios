6c50635fb25f680b7784b11cbf238f4062fa5fb9
Emre Sahin <github@emresult.com>
2021-03-04 18:13:10 +0300

    fixes for the preparation script

--- get-started/params-metrics-plots/assets/prepare.sh
+++ get-started/params-metrics-plots/assets/prepare.sh
@@ -28,72 +28,13 @@ set -o verbose
 :; git add data/.gitignore data/data.xml.dvc
 :; git commit -m "Add raw data to project"
 
-### Setup a data storage
-:; dvc remote add --default mystorage /tmp/data-storage
-:; git commit .dvc/config -m "Configure data storage"
+:; pip3 install -q wheel
+:; pip3 install -q -r src/requirements.txt
 
-### Push cached files to data storage
-:; dvc push -q
+:; git add *
+:; git commit -m "Beginning of the params-metrics-plots scenario"
 
-### Get the code
-:; wget -q https://code.dvc.org/get-started/code.zip
-:; unzip code.zip
-:; rm code.zip
-:; git add params.yaml src/
-:; git commit -m "Add source code files to repo"
+### Checking the DVC version
+:; dvc version
 
-### Install python requirements
-:; virtualenv -q -p python3 .env
-:; echo ".env/" >> .gitignore
-set +o verbose
-source .env/bin/activate
-set -o verbose
-:; pip install -qqq -r src/requirements.txt
-:; git add .gitignore
-:; git commit -m "Ignore virtualenv directory"
-
-### Stage: prepare
-:; dvc run \
-       -n prepare \
-       -d src/prepare.py \
-       -d data/data.xml \
-       -o data/prepared \
-       python \
-           src/prepare.py \
-           data/data.xml
-:; git add data/.gitignore dvc.yaml dvc.lock
-:; git commit -m "Create data preparation stage"
-:; dvc push -q
-
-### Stage: featurize
-:; dvc run \
-       -n featurize \
-       -d src/featurization.py \
-       -d data/prepared \
-       -o data/features \
-       python \
-           src/featurization.py \
-           data/prepared \
-           data/features
-:; git add data/.gitignore dvc.yaml dvc.lock
-
-### Stage: train
-:; dvc run \
-       -n train \
-       -d src/train.py \
-       -d data/features \
-       -o model.pkl \
-       python \
-           src/train.py \
-           data/features \
-           model.pkl
-:; git add data/.gitignore dvc.yaml dvc.lock
-:; git commit -m "Create featurization and training stages"
-:; dvc push -q
-
-### Reproducing the model
-:; sed -i data/data.xml -e '1d'
-:; dvc repro
-:; git add .
-:; git commit -m "Produced another version of model.pkl"
-:; dvc push -q
+### Ready ###
