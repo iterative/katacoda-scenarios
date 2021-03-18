c7d76453a6f25a49592d3b0a0db277aced66d42f
Emre Sahin <github@emresult.com>
2021-03-18 13:45:07 +0300

    added docker run command

--- get-started-dockerized/versioning/init.sh
+++ get-started-dockerized/versioning/init.sh
@@ -4,6 +4,8 @@
 PS1='\[\033[01;34m\]\w\[\033[00m\]$ \[\033[01;32m\]'
 trap 'echo -ne "\033[00m"' DEBUG
 
+docker run -it emresult/dvc-base
+
 # git
 # git config --global user.email "guest@example.com"
 # git config --global user.name "Guest User"
