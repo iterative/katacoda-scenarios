c9555daf2113e98c8a41ff26a480ce16d69af558
Emre Sahin <github@emresult.com>
2021-03-18 10:16:45 +0300

    start docker instead of installation

--- get-started-dockerized/versioning/install.sh
+++ get-started-dockerized/versioning/install.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-# install dvc
-# sudo wget https://dvc.org/deb/dvc.list \
-#           -O /etc/apt/sources.list.d/dvc.list
-# sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/dvc.list" \
-#     -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
-# sudo apt install dvc
-
-# instead of apt, use snap, it should be faster
-
-# snap install --classic dvc
-#
-# # install bash completion for dvc
-# dvc completion -s bash > /etc/bash_completion.d/dvc
+docker run -it emresult/dvc-base
