362cff8fc7e3f098143c92d829ed49e4fbeab2ba
Emre Sahin <github@emresult.com>
2021-03-18 11:14:54 +0300

    commented out most of init

--- get-started-dockerized/versioning/init.sh
+++ get-started-dockerized/versioning/init.sh
@@ -5,8 +5,8 @@ PS1='\[\033[01;34m\]\w\[\033[00m\]$ \[\033[01;32m\]'
 trap 'echo -ne "\033[00m"' DEBUG
 
 # git
-git config --global user.email "guest@example.com"
-git config --global user.name "Guest User"
+# git config --global user.email "guest@example.com"
+# git config --global user.name "Guest User"
 
 # wait for dvc and highlight to be installed
 # clear
@@ -17,33 +17,32 @@ git config --global user.name "Guest User"
 
 # We can just use snap to install in a user visible way. 
 
-clear
-:;: ===================================
-:;: Let\'s install DVC for this session
-:;: ===================================
-sleep 1
-
-snap install --classic dvc
-
+# clear
+# :;: ===================================
+# :;: Let\'s install DVC for this session
+# :;: ===================================
+# sleep 1
+#
+# snap install --classic dvc
+#
 # install bash completion for dvc
-dvc completion -s bash > /etc/bash_completion.d/dvc
-
+# dvc completion -s bash > /etc/bash_completion.d/dvc
+#
 # enable bash completion
-source /etc/bash_completion
-
-
-clear
-:;: ===================================
-:;: Let\'s create a new DVC project 
-:;: ===================================
-git init versioning
-cd versioning
-dvc init
-git add .
-git commit -m "Initial DVC files"
-
+# source /etc/bash_completion
+#
+# clear
+# :;: ===================================
+# :;: Let\'s create a new DVC project 
+# :;: ===================================
+# git init versioning
+# cd versioning
+# dvc init
+# git add .
+# git commit -m "Initial DVC files"
+#
 # clear screen
-clear
-
-tree .
-dvc version
+# clear
+#
+# tree .
+# dvc version
