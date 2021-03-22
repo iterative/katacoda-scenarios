f4d43b7b8d7d15f25d4ffe1ce7d53a04d7ef9f48
Emre Sahin <github@emresult.com>
2021-03-22 10:13:22 +0300

    updated init and install scripts for docker

--- get-started-dockerized/accessing/init.sh
+++ get-started-dockerized/accessing/init.sh
@@ -1,34 +1,7 @@
 #!/bin/bash
 
 # prompt
-PS1='\[\033[01;34m\]\w\[\033[00m\]$ \[\033[01;32m\]'
-trap 'echo -ne "\033[00m"' DEBUG
+# PS1='\[\033[01;34m\]\w\[\033[00m\]$ \[\033[01;32m\]'
+# trap 'echo -ne "\033[00m"' DEBUG
 
-# git
-git config --global user.email "guest@example.com"
-git config --global user.name "Guest User"
-
-# wait for DVC and highlight to be installed
-clear
-:;: ===================================
-:;: Please wait until DVC is installed.
-:;: ===================================
-until hash dvc &>/dev/null; do sleep 1; done
-
-# enable bash completion
-source /etc/bash_completion
-
-# git clone --branch 3-config-remote \
-#     https://github.com/iterative/example-get-started.git
-# cd example-get-started/
-# git reset –-hard 3-config-remote
-# cd ..
-
-git init project
-cd project
-
-# clear screen
-clear
-
-ls
-dvc version
+docker run -it --name dvcaccessing --mount type=bind,source="/root/project",target="/root/project" emresult/dvc-gs-accessing
