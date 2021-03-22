9a4156af1eb213280f9ea9dac3a4bd73fe632e18
Emre Sahin <github@emresult.com>
2021-03-22 10:21:26 +0300

    Updated scripts for dockerized Stages scenario

--- get-started-dockerized/stages/init.sh
+++ get-started-dockerized/stages/init.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 
 # prompt
-PS1='\[\033[01;34m\]\w\[\033[00m\]$ \[\033[01;32m\]'
-trap 'echo -ne "\033[00m"' DEBUG
+# PS1='\[\033[01;34m\]\w\[\033[00m\]$ \[\033[01;32m\]'
+# trap 'echo -ne "\033[00m"' DEBUG
 
-# git
-git config --global user.email "guest@example.com"
-git config --global user.name "Guest User"
-
-# wait for dvc and highlight to be installed
-clear
-:;: ===================================
-:;: Please wait until DVC is installed.
-:;: ===================================
-until hash dvc &>/dev/null; do sleep 1; done
-
-# enable bash completion
-source /etc/bash_completion
-
-# clear screen
-clear
-
-cd project
-# auto-play preparation steps
-DELAY=0 play prepare.sh
+docker run -it --name dvcstages --mount type=bind,source="/root/project",target="/root/project" emresult/dvc-gs-stages
