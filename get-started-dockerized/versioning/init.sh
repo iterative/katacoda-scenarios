da67116a183b2fd4025ec5cb0453d403857bb536
Emre Sahin <github@emresult.com>
2021-03-22 13:19:43 +0300

    added populated volume for versioning

--- get-started-dockerized/versioning/init.sh
+++ get-started-dockerized/versioning/init.sh
@@ -4,4 +4,6 @@
 # PS1='\[\033[01;34m\]\w\[\033[00m\]$ \[\033[01;32m\]'
 # trap 'echo -ne "\033[00m"' DEBUG
 
-docker run -it --name dvcversioning --mount type=volume,source="/root/project",target="/root/project" emresult/dvc-gs-versioning
+docker volume create project
+docker run -it --name dvcversioning -v project:/root/project emresult/dvc-gs-versioning
+ln -s /var/lib/docker/volumes/project/_data /root/project
