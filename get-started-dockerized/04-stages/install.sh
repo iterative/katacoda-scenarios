ae2ee7fd598d90cf07c93ac5dc4fc7d0dc45ae89
Emre Sahin <github@emresult.com>
2021-03-23 21:55:21 +0300

    renamed all directories and updated start scripts to run container

---
+++ get-started-dockerized/04-stages/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export CONTAINER="emresult/dvc-gs-stages"
+
+docker volume create project
+
+if [ -e /root/project ] ; then 
+    rm -rf /root/project
+fi
+ln -s /var/lib/docker/volumes/project/_data /root/project
+
+docker run -d -it --name dvc -v project:/root/project "$CONTAINER"
+
+touch /tmp/docker-ready
