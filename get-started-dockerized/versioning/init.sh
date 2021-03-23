12348844d3c9a239789f91679ee06bb3b7e9d241
Emre Sahin <github@emresult.com>
2021-03-23 21:10:28 +0300

    changed echo to :;:

--- get-started-dockerized/versioning/init.sh
+++ get-started-dockerized/versioning/init.sh
@@ -6,10 +6,10 @@ set +vx
 PS1='\[\033[01;34m\]\w\[\033[00m\]$ \[\033[01;32m\]'
 trap 'echo -ne "\033[00m"' DEBUG
 
-echo "###########################################"
-echo "## INSTALLING CONTAINER FOR THE SCENARIO ##"
-echo "## $ docker run -it dvcorg/gs-versioning ##"
-echo "###########################################"
+:;: "###########################################"
+:;: "## INSTALLING CONTAINER FOR THE SCENARIO ##"
+:;: "## $ docker run -it dvcorg/gs-versioning ##"
+:;: "###########################################"
 until [ -f /tmp/docker-ready ] ; do echo -n "." ; sleep 1 ; done
 
 docker attach dvc
