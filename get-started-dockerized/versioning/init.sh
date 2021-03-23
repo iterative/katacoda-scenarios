56fc5bba76933fdef623c07c58418232c27bc294
Emre Sahin <github@emresult.com>
2021-03-23 20:59:13 +0300

    moved docker init to background

--- get-started-dockerized/versioning/init.sh
+++ get-started-dockerized/versioning/init.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 # prompt
-# PS1='\[\033[01;34m\]\w\[\033[00m\]$ \[\033[01;32m\]'
-# trap 'echo -ne "\033[00m"' DEBUG
+PS1='\[\033[01;34m\]\w\[\033[00m\]$ \[\033[01;32m\]'
+trap 'echo -ne "\033[00m"' DEBUG
 
 echo "###########################################"
 echo "## INSTALLING CONTAINER FOR THE SCENARIO ##"
 echo "## $ docker run -it dvcorg/gs-versioning ##"
 echo "###########################################"
+until [ -f /tmp/docker-ready ] ; do echo -n "." ; sleep 1 ; done
 
-docker volume create project
-
-if [ -e /root/project ] ; then 
-    rm -rf /root/project
-fi
-ln -s /var/lib/docker/volumes/project/_data /root/project
-
-docker run -it --name dvcversioning -v project:/root/project emresult/dvc-gs-versioning
+docker attach dvc
