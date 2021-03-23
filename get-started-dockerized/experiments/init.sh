fa96fe72aa0c103a1f3c9ddf2fd430a1d8954e39
Emre Sahin <github@emresult.com>
2021-03-23 21:55:21 +0300

    renamed all directories and updated start scripts to run container

--- get-started-dockerized/experiments/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# prompt
-# PS1='\[\033[01;34m\]\w\[\033[00m\]$ \[\033[01;32m\]'
-# trap 'echo -ne "\033[00m"' DEBUG
-
-docker run -it --name dvcexperiments --mount type=bind,source="/root/project",target="/root/project" emresult/dvc-gs-experiments
--- get-started-dockerized/experiments/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# We containerized all requirements, nothing to install
