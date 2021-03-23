7b6be9ee653bd75c237c55f70d6858cc5aa1a6b9
Emre Sahin <github@emresult.com>
2021-03-23 21:23:07 +0300

    added clear statements

--- get-started-dockerized/versioning/init.sh
+++ get-started-dockerized/versioning/init.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-set +vx
-
 # prompt
 PS1='\[\033[01;34m\]\w\[\033[00m\]$ \[\033[01;32m\]'
 trap 'echo -ne "\033[00m"' DEBUG
+
+clear 
 
 :;: "###########################################"
 :;: "## INSTALLING CONTAINER FOR THE SCENARIO ##"
 :;: "## $ docker run -it dvcorg/gs-versioning ##"
 :;: "###########################################"
 until [ -f /tmp/docker-ready ] ; do echo -n "." ; sleep 1 ; done
+
+clear
 
 docker attach dvc
