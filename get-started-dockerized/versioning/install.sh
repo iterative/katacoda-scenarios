9ab0b1c90a4bdf2b56828b4c71d1097d5eac54de
Emre Sahin <github@emresult.com>
2021-03-23 20:59:13 +0300

    moved docker init to background

--- get-started-dockerized/versioning/install.sh
+++ get-started-dockerized/versioning/install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 # We containerized all requirements, nothing to install
+
+docker volume create project
+
+if [ -e /root/project ] ; then 
+    rm -rf /root/project
+fi
+ln -s /var/lib/docker/volumes/project/_data /root/project
+
+docker pull emresult/dvc-gs-versioning
+
+docker run -d --name dvc -v project:/root/project emresult/dvc-gs-versioning
+
+touch /tmp/docker-ready
