0ffe5daa142215edb32abcaff044f65040807038
Emre Sahin <github@emresult.com>
2021-03-25 22:49:31 +0300

    changed the ln command to test the editor behavior

--- get-started-dockerized/03-accessing/install.sh
+++ get-started-dockerized/03-accessing/install.sh
@@ -7,7 +7,7 @@ docker volume create example-get-started
 if [ -e /root/example-get-started ] ; then 
     rm -rf /root/example-get-started
 fi
-ln -s /var/lib/docker/volumes/example-get-started/_data /root/example-get-started
+ln /var/lib/docker/volumes/example-get-started/_data /root/example-get-started
 
 docker run -d -it --name dvc -v example-get-started:/root/example-get-started "$CONTAINER"
 
